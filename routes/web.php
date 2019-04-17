589ef18ebf8edbce829d42c31289016b0facf014
Rafi Aqnan Pebrian  Rafi Aqnan Pebrian <rafi.aqnan@gmail.com>
2019-04-17 13:46:55 +0700

    input data

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::get('/input', 'learnlaravelController@input');
+
+Route::post('/proses', 'learnlaravelController@proses');
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+*/
